a8ac89c64e97b3040733e6c0a96ddf67b9ad6dae
EQalaizer <vdsihjjkdhfihg457@gmail.com>
2024-08-13 15:43:38 +0300

    21
    12

---
+++ header.php
@@ -0,0 +1,14 @@
+
+
+<div class ="header">
+	<div class ="container">
+		<h1>Shapochka</h1>
+		<nav>
+			<ul>
+				<li><a href="#">SSILOCHKA</a></li>
+				<li><a href="#">esho odna</a></li>
+				<li><a href="#">opat nahoi</a></li>
+			</ul>
+		</nav>
+	</div>
+</div>
